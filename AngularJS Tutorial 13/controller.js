79e808c84ad76559e28e29a37cae0bd274f129b9
ofri <ofriofriofri@gmail.com>
2017-01-23 17:49:12 +0200

    tutorial 13 commit

--- AngularJS Tutorial 13/controller.js
+++ AngularJS Tutorial 13/controller.js
@@ -3,7 +3,10 @@ var app = angular.module('mainApp',['ngRoute']);
 app.config(function($routeProvider){
     $routeProvider
     .when('/',{
-        templateUrl: 'page1.html'
+        
+        templateUrl: 'page1.html',
+//        template: '<b>what</b>'
+//see what happen when u remove the comment above
     })
     .when('/helloUser',{
         templateUrl: 'page2.html'
